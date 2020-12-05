cc62a689f815f23e66de55ad0ff59089fc11bdd5
Densuke-fitness <densukefitness2020@gmail.com>
2020-12-05 22:50:13 +0900

    .envの追加

--- app/flaskr/models/__init__.py
+++ app/flaskr/models/__init__.py
@@ -1,8 +1,13 @@
+import os
+
+from dotenv import load_dotenv
 import pandas as pd
 from pymongo import MongoClient
 
+load_dotenv()
+
 #MongoDBびローカル環境の指定 悪用防止のためパスワードは差し替えています。
-client = MongoClient("mongodb+srv://Densuke:<password>@optimze-portfolio-clust.pydh1.mongodb.net/stock_database?retryWrites=true&w=majority")
+client = MongoClient(f"mongodb+srv://Densuke:{os.environ['PASSWORD']}@optimze-portfolio-clust.pydh1.mongodb.net/stock_database?retryWrites=true&w=majority")
 #データベースの起動
 db = client['stock_database']
 #クエリ操作の際に使うファイル
